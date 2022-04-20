7771273e7fabe10c89ed5dd49027abb5ecfc7112
khursik <khursik.ev@gmail.com>
2022-04-20 20:25:50 +0300

    init commit

---
+++ src/main/java/ru/sbt/uploadfile/FileUploadRestfulWebServiceApplication.java
@@ -0,0 +1,14 @@
+package ru.sbt.uploadfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FileUploadRestfulWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileUploadRestfulWebServiceApplication.class, args);
+    }
+
+}
