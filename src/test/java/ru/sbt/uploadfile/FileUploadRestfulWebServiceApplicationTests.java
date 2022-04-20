8e85b28898c8b220e48edac5030c0ec21c195a2e
khursik <khursik.ev@gmail.com>
2022-04-20 20:40:13 +0300

    hw_2

---
+++ src/test/java/ru/sbt/uploadfile/FileUploadRestfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sbt.uploadfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploadRestfulWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
